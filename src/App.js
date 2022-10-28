afdca344c55e5d961577aa5d55f6fae48d500a7a
daniel-monsalve-villegas <unbigote@hush.com>
2022-10-28 11:32:08 -0400

    feat: converter working and showing result

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ function App() {
 
   const [value, setValue] = useState(0);
   const [result, setResult] = useState();
-  const [optionConvert, setOptionConvert] = useState();
+  const [optionConvert, setOptionConvert] = useState("fahrenheit");
+  const [tempShow, setTempShow] = useState();
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
-  //const handleConvert = () => {
-    //setCelsius(value - 273.15);
-  //};
-
   const handleSelectOption = (event) => {
+    event.preventDefault();
     setOptionConvert(event.target.value);
+    console.log(event.target.value);
   };
 
   const handleConvert = () => {
     if(optionConvert === 'fahrenheit') {
       setResult((value - 32) * (5/9));
+      setTempShow("°C");
     } else {
-      setResult(value - 273.15)
-    }
+      setResult((value * (9/5)) + 32);
+      setTempShow("°F");
   };
-
+}
 
   return (
     <div className="App">
@@ -36,11 +36,11 @@ function App() {
       <div>
         <select onChange={handleSelectOption}>
           <option value="fahrenheit">Fahrenheit</option>
-          <option value="celsius">Kelvin</option>
+          <option value="celsius">Celsius</option>
         </select>
         <button onClick={handleConvert}>Convert</button>
       </div>
-      <p>Result: {result} result</p>
+      <p>Result: {result} {tempShow}</p>
     </div>
   );
 }
